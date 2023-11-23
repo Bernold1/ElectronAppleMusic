af36a86e17d8a74d703641cb0211e1a1579114f1
Chris Bernold <christopherbernold@icloud.com>
2023-11-23 10:58:53 +0100

    Removed app

--- buildApplication.js
+++ buildApplication.js
@@ -4,7 +4,7 @@ const location = process.argv.slice(2).join("");
 const url = `https://music.apple.com/${location}/browse`;
 const appName = "apple-music-electron";
 
-const command = `nativefier --name '${appName}' '${url}'`;
+const command = `nativefier --name '${appName}' --lang 'da-DK' '${url}'`;
 
 exec(command, (error, stdout, stderr) => {
   if (error) {
